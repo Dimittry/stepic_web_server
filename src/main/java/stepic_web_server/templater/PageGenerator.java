05845bb14cc010993fa57bb657d218f1e9d8ce91
dmitriy <demar.07@gmail.com>
2016-12-22 09:43:32 +0900

    Has been changed the place where the files of templates are located.

--- src/main/java/stepic_web_server/templater/PageGenerator.java
+++ src/main/java/stepic_web_server/templater/PageGenerator.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class PageGenerator {
     private static final String HTML_DIR = "templates";
@@ -25,7 +26,8 @@ public class PageGenerator {
     public String getPage(String filename, Map<String, Object> data) {
         Writer stream = new StringWriter();
         try {
-            Template template = cfg.getTemplate(HTML_DIR + File.separator + filename);
+            cfg.setClassForTemplateLoading(this.getClass(), File.separator + HTML_DIR + File.separator );
+            Template template = cfg.getTemplate(filename);
             template.process(data, stream);
         } catch(IOException | TemplateException e) {
             e.printStackTrace();
